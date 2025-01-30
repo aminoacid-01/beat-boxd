8caf4666051d03e86e630035f73e4c8f3b759263
Amina <154568053+aminoacid-01@users.noreply.github.com>
2025-01-30 20:05:26 +0000

    add debug to env.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u&a8*)^+&hmw*$0nkzlz(l8@+0q&4+t!0zdzxxb92*a-6@re=h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "False"
-
+DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 't', 'yes')
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '.herokuapp.com',
