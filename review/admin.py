9880d1f12daf727a005e255bf3b0dcf0628b535b
Amina <154568053+aminoacid-01@users.noreply.github.com>
2025-01-31 17:45:58 +0000

    install and hook up summernote to the review model

--- review/admin.py
+++ review/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Album, Review
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+    list_display = ('heading', 'slug', 'status')
+    search_fields = ['heading', 'body']
+    list_filter = ('status',)
+    summernote_fields = ('body',)
 
 # Register your models here.
 admin.site.register(Album)
-admin.site.register(Review)
-
