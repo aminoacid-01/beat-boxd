e7cd1eeadce6bec8f4e8bf19e026df9359cb57b6
Amina <154568053+aminoacid-01@users.noreply.github.com>
2025-01-31 15:36:53 +0000

    added links to home_page on nav bar

--- review/urls.py
+++ review/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.review_list, name='review_list'),
+    path('', views.review_list, name='home_page'),
     path('reviews/<slug:slug>/', views.review_detail, name='review_detail'),
     path('create/', views.create_review, name='create_review'),
 ]
