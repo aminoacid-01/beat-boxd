97ff96b1f43cffadd5daaf801eb47c215f551091
Amina <154568053+aminoacid-01@users.noreply.github.com>
2025-02-01 16:40:42 +0000

    add docstring to function in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -5,16 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const imageUrlInput = document.getElementById('id_image_url'); // Get the image_url input element
     const descriptionInput = document.getElementById('id_description'); // Get the description input element
     const albumDropdown = document.getElementById('id_album_dropdown');  // Get the dropdown element
-
+    // call the hideDescriptionAndImageUrl function
     hideDescriptionAndImageUrl(descriptionInput, imageUrlInput);
 });
 
 function hideDescriptionAndImageUrl(descriptionInput, imageUrlInput) {
-    // Hide the description and image URL input elements
+    /**
+     *  Function to hide the description and image URL input elements and their labels
+     * @param {Element} descriptionInput - The description input element
+     * @param {Element} imageUrlInput - The image URL input element
+     * @returns {void}
+     */
     descriptionInput.style.display = 'none';
     imageUrlInput.style.display = 'none';
-
-    // Hide the labels for the description and image URL input elements
     const descriptionLabel = document.querySelector('label[for="id_description"]');
     const imageUrlLabel = document.querySelector('label[for="id_image_url"]');
     if (descriptionLabel) descriptionLabel.style.display = 'none';
