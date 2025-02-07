9edb847997011fc10496d4635bcd19eea49f165c
Amina <154568053+aminoacid-01@users.noreply.github.com>
2025-02-07 14:08:49 +0000

    Refactor admin.py for improved readability and consistency in list_display formatting

--- review/admin.py
+++ review/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from .models import Album, Review, Comment, Rating
 from django_summernote.admin import SummernoteModelAdmin
 
+
 class ReviewInline(admin.TabularInline):
     model = Review
     fields = ('author', 'heading', 'excerpt')
     readonly_fields = ('author', 'heading', 'excerpt',)
     extra = 0
+
 
 class CommentInline(admin.TabularInline):
     model = Comment
@@ -14,26 +16,22 @@ class CommentInline(admin.TabularInline):
     readonly_fields = ('author', 'body')
     extra = 0
 
-class RatingInline(admin.TabularInline):
-    model = Rating
-    fields = ('user', 'value')
-    readonly_fields = ('author', 'value')
-    extra = 0
-
-class AlbumInline(admin.TabularInline):
-    model = Album
-    fields = ('title', 'artist')
-    readonly_fields = ('title', 'artist')
-    extra = 0
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
-    list_display = ('heading', 'slug', 'status', 'created_on', 'updated_on', 'author')
+    list_display = (
+        'heading',
+        'slug',
+        'status',
+        'created_on',
+        'updated_on',
+        'author')
     search_fields = ['heading', 'body']
     list_filter = ('status',)
     summernote_fields = ('body',)
     inlines = [
         CommentInline]
+
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
@@ -41,9 +39,16 @@ class RatingAdmin(admin.ModelAdmin):
     list_filter = ('value', 'album',)
 
 # Register your models here.
+
+
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
-    list_display = ('title', 'artist', 'release_date', 'average_rating', 'review_count')
+    list_display = (
+        'title',
+        'artist',
+        'release_date',
+        'average_rating',
+        'review_count')
     search_fields = ['title', 'artist']
     list_filter = ('artist',)
     inlines = [ReviewInline]
@@ -59,7 +64,7 @@ class AlbumAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(SummernoteModelAdmin):
-    list_display = ('author','body','created_on')
+    list_display = ('author', 'body', 'created_on')
     search_fields = ['name', 'body']
-    list_filter = ('created_on','approved')
+    list_filter = ('created_on', 'approved')
     summernote_fields = ('body',)
