3c53ec2f93efe6adf224ae0f330b183e0703a152
Amina <154568053+aminoacid-01@users.noreply.github.com>
2025-02-07 14:05:25 +0000

    Refactor forms.py to pass validation

--- review/forms.py
+++ review/forms.py
@@ -2,12 +2,23 @@ from django import forms
 from .models import Album, Review, Comment, Rating
 from django_summernote.widgets import SummernoteWidget
 
+
 class AlbumForm(forms.ModelForm):
-    existing_album = forms.ModelChoiceField(queryset=Album.objects.all(), required=False, empty_label="Select an existing album")
+    existing_album = forms.ModelChoiceField(
+        queryset=Album.objects.all(),
+        required=False,
+        empty_label="Select an existing album"
+    )
 
     class Meta:
         model = Album
-        fields = ['existing_album', 'title', 'artist', 'image_url', 'description']
+        fields = [
+            'existing_album',
+            'title',
+            'artist',
+            'image_url',
+            'description'
+        ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,6 +26,7 @@ class AlbumForm(forms.ModelForm):
         self.fields['artist'].required = False
         self.fields['image_url'].required = False
         self.fields['description'].required = False
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
@@ -27,6 +39,7 @@ class ReviewForm(forms.ModelForm):
         widgets = {
             'body': SummernoteWidget(),
         }
+
 
 class RatingForm(forms.ModelForm):
     class Meta:
@@ -42,6 +55,7 @@ class RatingForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['value'].required = True
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
