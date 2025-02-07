b7172ace6162bd92ca270103fcd61fcbfd656d7c
Amina <154568053+aminoacid-01@users.noreply.github.com>
2025-02-07 14:01:20 +0000

    Refactor URL patterns in review/urls.py to pass validation

--- review/urls.py
+++ review/urls.py
@@ -3,12 +3,20 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.recent_review_list, name='home_page'),
-    path('api/albums/<int:album_id>/', views.album_detail, name='album_detail'),
-    path('create/', views.create_review, name='create_review'),
-    path('reviews/', views.review_list, name='review_list'),
-    path('reviews/user', views.user_review_list, name='user_reviews'),
-    path('reviews/<slug:slug>/', views.review_detail, name='review_detail'),
-    path('reviews/<slug:slug>/delete/', views.delete_review, name='delete_review'),
-    path('reviews/<slug:slug>/edit/', views.edit_review, name='edit_review'),
+    path('', views.recent_review_list,
+         name='home_page'),
+    path('api/albums/<int:album_id>/', views.album_detail,
+         name='album_detail'),
+    path('create/', views.create_review,
+         name='create_review'),
+    path('reviews/', views.review_list,
+         name='review_list'),
+    path('reviews/user', views.user_review_list,
+         name='user_reviews'),
+    path('reviews/<slug:slug>/', views.review_detail,
+         name='review_detail'),
+    path('reviews/<slug:slug>/delete/', views.delete_review,
+         name='delete_review'),
+    path('reviews/<slug:slug>/edit/', views.edit_review,
+         name='edit_review'),
 ]
